12406d5f281fb4c2a7897ac8a9bc228e284ae67b
SatrioBayu <sbayu875@gmail.com>
2022-03-21 12:54:34 +0700

    Mengedit navbar, css, dan js

---
+++ script.js
@@ -0,0 +1,8 @@
+function openNav() {
+  document.getElementById("sidebar").style.width = "200px";
+  //   document.getElementById("main").style.marginLeft = "200px";
+}
+function closeNav() {
+  document.getElementById("sidebar").style.width = "0";
+  //   document.getElementById("main").style.marginLeft = "0";
+}
