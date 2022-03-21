f2f3d56e05ab73af501843f8212f47a67ddc0e2a
SatrioBayu <sbayu875@gmail.com>
2022-03-21 16:55:37 +0700

    Mengedit navbar, menambahkan asset gambar, css, dan js

--- script.js
+++ script.js
@@ -4,5 +4,9 @@ function openNav() {
 }
 function closeNav() {
   document.getElementById("sidebar").style.width = "0";
-  //   document.getElementById("main").style.marginLeft = "0";
+  document.getElementById("main").style.marginLeft = "0";
+}
+function openNavMarg() {
+  document.getElementById("sidebar").style.width = "200px";
+  document.getElementById("main").style.marginLeft = "200px";
 }
